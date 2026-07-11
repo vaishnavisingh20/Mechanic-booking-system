0432cd08088e1e149a491ca1d5e2fdcada8a7a24
JahnaviSingh09 <jahnavis703@gmail.com>
2026-07-11 12:32:05 +0530

    Fix ESLint config for Vercel deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  nextVitals,
+  nextTs,
 
   globalIgnores([
     ".next/**",
